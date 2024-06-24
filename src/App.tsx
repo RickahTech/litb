cfd3a67ab342e9d8d2f1d59e7a46aab725a6bd67
RickahTech <ericadossantos27@gmail.com>
2024-06-24 21:06:56 +0100

    l

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,71 @@
-// src/App.tsx
+import { useEffect, useState } from "react";
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+import Home from "./pages/home";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './pages/home';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+
+const client = generateClient<Schema>();
+
+type TodoType = Schema["Todo"]["type"];
 
 function App() {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+
+  useEffect(() => {
+    const subscription = client.models.Todo.observeQuery().subscribe({
+      next: (data: { items: TodoType[] }) => setTodos([...data.items]),
+    });
+    // Cleanup subscription on unmount
+    return () => subscription.unsubscribe();
+  }, []);
+
+  function createTodo() {
+    const content = window.prompt("Todo content");
+    if (content) {
+      client.models.Todo.create({ content });
+    }
+  }
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
+  }
+
   return (
-    <Router>
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <Home />
-        </Route>
-    </Router>
+    <Authenticator>
+      
+      {({ signOut }) => (
+        <main>
+          <Router>
+            <Route path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <Home />
+            </Route>
+        </Router>
+          <h1>My todos</h1>
+          <button onClick={createTodo}>+ new</button>
+          <ul>
+            {todos.map((todo: TodoType) => (
+              <li onClick={() => deleteTodo(todo.id)}
+                  key={todo.id}>{todo.content || 'No content'}</li>
+            ))}
+          </ul>
+          <div>
+            🥳 App successfully hosted. Try creating a new todo.
+            <br />
+            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+              Review next step of this tutorial.
+            </a>
+          </div>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
