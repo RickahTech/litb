446d607b9a8548bed9eccf2ee46584feb59ed545
RickahTech <ericadossantos27@gmail.com>
2024-07-04 19:54:27 +0100

    1 multi page

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  build: {
-    rollupOptions: {
-      // Any additional Rollup options if necessary
-    },
+  resolve: {
+    alias: {
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@components': path.resolve(__dirname, 'src/components')
+    }
   },
+  plugins: [react()],
 });
